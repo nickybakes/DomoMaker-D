7601cd0f45015f7a18a06104bc5021d7c1d4515d
Nick Baker <nickmbaker1@gmail.com>
2022-11-06 15:46:02 -0500

    Got maker page working with react

--- server/router.js
+++ server/router.js
@@ -2,7 +2,8 @@ const controllers = require('./controllers');
 const mid = require('./middleware');
 
 const router = (app) => {
-  app.get('/getToken', mid.requiresSecure, mid.requiresSecure, controllers.Account.getToken);
+  app.get('/getToken', mid.requiresSecure, controllers.Account.getToken);
+  app.get('/getDomos', mid.requiresLogin, controllers.Domo.getDomos);
 
   app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
